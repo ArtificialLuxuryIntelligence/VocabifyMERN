5df0e2bb195802bd72ff4b8a669df2bddf49a961
ali <adgk.dev@gmail.com>
2020-03-01 13:06:37 +0100

    Heroku build scripts

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use("/api", apiRouter);
 
 //serve static assets in productions
 
-app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname, "client/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
