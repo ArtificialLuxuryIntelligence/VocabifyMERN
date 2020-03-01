b0f3a3ca5c631148928c907f72926835ba6dab77
ali <adgk.dev@gmail.com>
2020-03-01 12:48:26 +0100

    Heroku build scripts

--- app.js
+++ app.js
@@ -31,12 +31,11 @@ app.use("/api", apiRouter);
 
 //serve static assets in productions
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
 // app.use(express.static(path.join(__dirname, "./client/build")));
 
 // /*React root*/
