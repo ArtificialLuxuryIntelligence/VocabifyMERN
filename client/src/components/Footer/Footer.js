cffce25f517957ea9150b745e4b1d5ac6b635375
ali <adgk.dev@gmail.com>
2020-05-08 11:45:47 +0200

    footer => functional component

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,18 +1,7 @@
-import React, { Component } from "react";
-import { Link, Redirect } from "react-router-dom";
+import React from "react";
 import "./Footer.scss";
-// import axios from "axios";
-// import auth from "../../utils/auth";
 
-class Footer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return <div className="footer"></div>;
-  }
-}
-
+const Footer = () => {
+  return <div className="footer"></div>;
+};
 export default Footer;
